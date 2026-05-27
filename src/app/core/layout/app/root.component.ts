908054dae49cb44d96be818a804ff05204ec5def
amanimavu <amanimavu@gmail.com>
2026-05-27 12:21:41 +0300

    chore: move the scrollbar to the main element
    - remove it from the html element

--- src/app/core/layout/app/root.component.ts
+++ src/app/core/layout/app/root.component.ts
@@ -3,10 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { SideNavBar } from './sidenav.component';
 import { Header } from './header.component';
 @Component({
-	template: `<div class="relative min-h-screen bg-[#F8F9FF]">
+	template: `<div class="relative h-screen overflow-hidden bg-[#F8F9FF]">
 		<sidebar></sidebar>
 		<header></header>
-		<main class="pl-28 py-24 pr-8"><router-outlet /></main>
+		<main class="pl-28 pt-16 h-full">
+			<div class="pr-8 pt-8 pb-20 h-full overflow-y-auto scrollbar-thin relative">
+				<router-outlet />
+			</div>
+		</main>
 	</div>`,
 	imports: [RouterOutlet, Header, SideNavBar],
 })
