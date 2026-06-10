7ce70d626316e012c2c6d477bae029008ab05b8e
amanimavu <amanimavu@gmail.com>
2026-06-10 22:01:22 +0300

    chore: add primeng aura theme to app config

--- projects/kumbuka-website/src/app/app.config.ts
+++ projects/kumbuka-website/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -9,5 +11,10 @@ export const appConfig: ApplicationConfig = {
 		provideBrowserGlobalErrorListeners(),
 		provideRouter(routes),
 		provideClientHydration(withEventReplay()),
+		providePrimeNG({
+			theme: {
+				preset: Aura,
+			},
+		}),
 	],
 };
