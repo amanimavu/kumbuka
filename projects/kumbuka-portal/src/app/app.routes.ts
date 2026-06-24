3119fa7744bd27de830189977bd7ffbaf924669a
amanimavu <amanimavu@gmail.com>
2026-06-24 14:17:30 +0300

    refactor: remove AddUserPage component and its associated route

--- projects/kumbuka-portal/src/app/app.routes.ts
+++ projects/kumbuka-portal/src/app/app.routes.ts
@@ -3,13 +3,12 @@ import { AuthLayout } from './core/layout/admin/auth/auth.component';
 import { RootLayout } from './core/layout/admin/root.component';
 import { LoginPage } from '@routes/login/login.page';
 import { UserManagementPage } from '@routes/user-management/user-management.page';
-import { AddUserPage } from '@routes/add-user/add-user.page';
 import { UserDetailsPage } from '@routes/user-details/user-details.page';
 import { NotFoundPage } from '@routes/not-found/not-found.page';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-	{ path: '', redirectTo: 'auth', pathMatch: 'full' },
+	{ path: '', redirectTo: 'auth/login', pathMatch: 'full' },
 	{ path: 'auth', redirectTo: 'auth/login', pathMatch: 'full' },
 	{ path: 'admin', redirectTo: 'admin/user-management', pathMatch: 'full' },
 	{
@@ -28,7 +27,6 @@ export const routes: Routes = [
 				component: UserManagementPage,
 				title: 'User Management',
 			},
-			{ path: 'add-user', component: AddUserPage, title: 'Add User' },
 			{
 				path: 'user-details/:id',
 				component: UserDetailsPage,
